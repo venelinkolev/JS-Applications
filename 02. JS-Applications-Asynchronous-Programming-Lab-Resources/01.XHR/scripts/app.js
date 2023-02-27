8278f09e2fa74b1aa38095d891da5e1bd3988d85
venelinkolev <venelinkolev@gmail.com>
2023-02-27 17:22:28 +0200

    Promises

--- 02. JS-Applications-Asynchronous-Programming-Lab-Resources/01.XHR/scripts/app.js
+++ 02. JS-Applications-Asynchronous-Programming-Lab-Resources/01.XHR/scripts/app.js
@@ -1,3 +1,24 @@
 function loadRepos() {
-   console.log("TODO...");
+  const loadBtn = document.querySelector('button');
+  const result = document.getElementById('res');
+
+  loadBtn.addEventListener('click', requestFunc);
+
+  //debugger;
+  let url = 'https://api.github.com/users/testnakov/repos';
+
+  const httpRequest = new XMLHttpRequest();
+
+  function requestFunc() {
+    httpRequest.addEventListener('readystatechange', () => {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        result.textContent = httpRequest.responseText;
+      }
+    });
+
+    httpRequest.open('GET', url);
+    httpRequest.send();
+
+    console.log('TODO...');
+  }
 }
--- 02. JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
+++ 02. JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
@@ -1,3 +1,30 @@
 function loadRepos() {
-	console.log("TODO...");
+  const loadBtn = document.querySelector('button');
+
+  const userName = document.getElementById('username').value;
+  const repos = document.getElementById('repos');
+
+  let url = `https://api.github.com/users/${userName}/repos`;
+
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        console.log(res);
+        console.log(res.status, res.statusText);
+        return Promise.reject(`${res.status}: ${res.statusText}`);
+      } else {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      repos.replaceChildren();
+      for (let line of data) {
+        let li = document.createElement('li');
+        li.innerHTML = `<a href="${line.html_url}"> ${line.full_name} </a>`;
+        repos.appendChild(li);
+      }
+    })
+    .catch((err) => {
+      repos.innerHTML = `<p>${err}</p>`;
+    });
 }
